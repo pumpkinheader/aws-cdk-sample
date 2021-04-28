f7257dc9039c2d97d222ebb0de657e28fac1e32f
pumpkinheader <pumpkinde@gmail.com>
2021-04-28 09:22:14 +0900

    use ecr image

--- ecs-sample/lib/ecs-sample-stack.ts
+++ ecs-sample/lib/ecs-sample-stack.ts
@@ -2,13 +2,22 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ecsPatterns from "@aws-cdk/aws-ecs-patterns";
-import { ApplicationProtocol, ApplicationProtocolVersion } from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as elb from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as ecr from "@aws-cdk/aws-ecr"
+import * as iam from "@aws-cdk/aws-iam"
+import { ManagedPolicy, Policy } from '@aws-cdk/aws-iam';
 
 export class EcsSampleStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // The code that defines your stack goes here
+        const repository = ecr.Repository.fromRepositoryName(this, 'devSandboxEcstestRepo', 'dev-sandbox-ecstest-repo')
+
+        const ecscodedeployrole = new iam.Role(this, "ecs-test-codedeploy-role", {
+            assumedBy: new iam.ServicePrincipal("codedeploy.amazonaws.com")
+        }).addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AWSCodeDeployRoleForECS"))
+
         const vpc = new ec2.Vpc(this, "dev-sandbox-ecstest-vpc", {
             cidr: "10.1.0.0/16",
             subnetConfiguration: [
@@ -30,16 +39,17 @@ export class EcsSampleStack extends cdk.Stack {
         });
         const taskDefinition = new ecs.FargateTaskDefinition(this, 'DevSandboxSampleTaskDefinition');
         const container = taskDefinition.addContainer('DevSandboxSampleWebContainer', {
-            image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+            image: ecs.ContainerImage.fromEcrRepository(repository),
         });
         container.addPortMappings({
             containerPort: 80
         });
 
+        // 何故か３０分たっても終わらない。。要検証。リポジトリにimageがなくて待ってるかも。最大１時間待つはず。
         const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'DevSandboxSampleService', {
             cluster: cluster,
-            cpu: 512,
-            memoryLimitMiB: 1024,
+            cpu: 256,
+            memoryLimitMiB: 512,
             desiredCount: 1,
             // やるならACMで証明書が必要だよ。targetProtocol: ApplicationProtocol.HTTPS,
             taskDefinition: taskDefinition
